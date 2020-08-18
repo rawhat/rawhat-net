179eeb62b03a2f62e0e3067742c55a041be73be3
Alex Manning <alex41290@gmail.com>
2020-08-18 20:07:42 +0000

    changes from long ago

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var router = require('koa-router')();
 var compress = require('koa-compress');
 app.use(compress())
 
-var serve = require('koa-static');
-app.use(serve('./static'));
+//var serve = require('koa-static');
+//app.use(serve('./static'));
 
 var views = require('koa-views');
 app.use(views(__dirname + '/views', {
